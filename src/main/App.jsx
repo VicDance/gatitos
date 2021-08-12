fed56dfba66869bab354cc269c8bde7e72eba94c
INT\victoria.garcia <victoria.garcia@solera.com>
2021-08-12 20:20:09 +0200

    creando primeros archivos

---
+++ src/main/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import HeaderComponent from '../header/HeaderComponent';
+
+
+const App = () => {
+    return(
+        <HeaderComponent />
+    );
+}
+
+export default App;
